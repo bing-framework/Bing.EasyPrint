1b30c2725a89c2bc681cc543cde1fb7cb70a620e
jian玄冰 <jianxuanhuo1@126.com>
2020-10-23 23:30:41 +0800

    feat: 初始化项目以及定义缓冲区写入器

---
+++ src/Bing.EasyPrint/Extensions/Extensions.IBufferWriter.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Text;
+
+// ReSharper disable once CheckNamespace
+namespace Bing.EasyPrint
+{
+    /// <summary>
+    /// 缓冲区写入器(<see cref="IBufferWriter"/>) 扩展
+    /// </summary>
+    public static class BufferWriterExtensions
+    {
+        /// <summary>
+        /// 转换为16进制
+        /// </summary>
+        /// <param name="bufferWriter">缓冲区写入器</param>
+        public static string ToHex(this IBufferWriter bufferWriter)
+        {
+            var bytes = bufferWriter.GetBytes();
+            if (!bytes.Any())
+                return string.Empty;
+            var result = new StringBuilder();
+            foreach (var b in bytes)
+                result.AppendFormat("{0:x2}", b);
+            return result.Replace("-", "").ToString();
+        }
+
+    }
+}
---
+++ src/Bing.EasyPrint/IBufferWriter.cs
@@ -0,0 +1,38 @@
+﻿namespace Bing.EasyPrint
+{
+    /// <summary>
+    /// 缓冲区写入器
+    /// </summary>
+    /// <typeparam name="T">类型</typeparam>
+    public interface IBufferWriter<out T>
+    {
+        /// <summary>
+        /// 写入
+        /// </summary>
+        /// <param name="value">字节数组</param>
+        T Write(byte[] value);
+
+        /// <summary>
+        /// 写入
+        /// </summary>
+        /// <param name="value">字符串</param>
+        T Write(string value);
+
+        /// <summary>
+        /// 清空内容
+        /// </summary>
+        T Clear();
+
+        /// <summary>
+        /// 获取二进制数组
+        /// </summary>
+        byte[] GetBytes();
+    }
+
+    /// <summary>
+    /// 缓冲区写入器
+    /// </summary>
+    public interface IBufferWriter : IBufferWriter<IBufferWriter>
+    {
+    }
+}
